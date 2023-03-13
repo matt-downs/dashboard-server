997c2d1f30189a40346e6cbf804361886b4c5b6c
matt-downs <ma77hew101@gmail.com>
2023-03-14 09:09:03 +1000

    Remove title from random dog

--- src/components/RandomDog.tsx
+++ src/components/RandomDog.tsx
@@ -6,9 +6,5 @@ export const RandomDog = () => {
   const data = useExternalDataContext();
   if (!data) return <span>error</span>;
 
-  return (
-    <DataWithTitle title="Dog of the day">
-      <img src={data.dog} style={{ maxWidth: "100%" }} />
-    </DataWithTitle>
-  );
+  return <img src={data.dog} style={{ maxWidth: "100%" }} />;
 };
